4441b63316c6ce42ac5d22cb73a66562e075c686
Alex Martin <gradient.am@gmail.com>
2017-10-09 00:09:57 -0700

    added end value only overload, wrapper function for implicit int ranges

--- main.cpp
+++ main.cpp
@@ -11,23 +11,21 @@ template<typename Type> void test()
 {
     high_resolution_clock::time_point t0 = high_resolution_clock::now();
 
-    for (auto i : xrange<Type>(0, 1000000))
-    {
+    for (auto i : xrange<Type>(0, 1000000)) {
         continue;
     }
 
     high_resolution_clock::time_point t1 = high_resolution_clock::now();
 
-    double time_elapsed = duration_cast<microseconds>(t1 - t0).count();
+    long long time_elapsed = duration_cast<microseconds>(t1 - t0).count();
 
     std::cout << "Time: " << time_elapsed << " us" << std::endl;
 }
 
 int main()
 {
-    //test<int>();
-    test<long long>();
     test<int>();
+    test<long long>();
     test<float>();
     test<double>();
     test<long double>();
--- xrange.h
+++ xrange.h
@@ -1,6 +1,6 @@
 #pragma once
 
-template<typename Type = int> class xrange
+template<typename Type = int> class Xrange
 {
 private:
     Type m_start;
@@ -11,13 +11,12 @@ public:
     class iterator;
     class end_iterator;
 
-    xrange(Type start, Type end, Type step = 1) : m_start(start), m_end(end), m_step(step) {}
+    Xrange(Type start, Type end, Type step) : m_start(start), m_end(end), m_step(step) {}
     iterator begin()    { return iterator(m_start, m_step); }
     iterator end()      { return iterator(m_end, 0); }
-    //end_iterator end() { return end_iterator(m_end); }
 };
 
-template<typename Type> class xrange<Type>::iterator
+template<typename Type> class Xrange<Type>::iterator
 {
 private:
     Type m_val;
@@ -30,12 +29,12 @@ public:
     Type operator*()                        { return m_val; }
 };
 
-template<typename Type> class xrange<Type>::end_iterator
+template<typename Type = int> Xrange<Type> xrange(Type start, Type end, Type step = 1)
 {
-private:
-    const Type& m_val;
+    return Xrange<Type>(start, end, step);
+}
 
-public:
-    explicit end_iterator(const Type& val) : m_val(val) {}
-    friend bool operator!=(iterator& it, const end_iterator& end_it) { return *it != end_it.m_val; }
-};
+template<typename Type = int> Xrange<Type> xrange(Type end)
+{
+    return Xrange<Type>(0, end, 1);
+}
